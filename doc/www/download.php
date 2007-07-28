4632f630a80b276bf377785010ad411b4b04e7f4
mheily <mheily@ad39ac92-8f34-0410-9d3c-65e2fc2d44a6>
2007-07-28 16:04:09 +0000

    Website redesign, no code changes

---
+++ doc/www/download.php
@@ -0,0 +1,11 @@
+<?php include "header.html"?>
+
+<h2>Download</h2>
+
+Source code releases are hosted by <a href="http://code.google.com">Google Code</a> and can be downloaded from the list below. 
+<p>
+
+<iframe src="http://code.google.com/p/naturalc/downloads/list"
+	style="width:100%; height: 100%; border: 0"></iframe>
+
+<?php include "footer.html"?>
